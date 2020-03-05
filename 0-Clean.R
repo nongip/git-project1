00bac76c0e114a107fb606c76d1d336e9acaa2e0
nongip <nongip@hotmail.it>
2020-03-05 17:17:18 +0100

    change storm completely

--- 0-Clean.R
+++ 0-Clean.R
@@ -3,10 +3,11 @@
 library(dplyr)
 library(lubridate)
 
-storms<-read.csv("storms.csv",header = FALSE)
+storms<-read.csv("storms.csv",header = TRUE)
+
 
 storms<-storms %>%
-        rename(month=V11,state=V8,type=V12,fakewind=V7) %>%
-        mutate(time=mdy_hm(storms$V19)) %>%
-        select(month,time,state,type,fakewind)
+        mutate(time=ymd_h(paste(year,month,day,hour))) %>%
+        select(name,year,time,lat,long,pressure,wind,status,category)
+
 write.csv(storms,file="storms1.csv",row.names=FALSE)
